a3a578e1f1e1b2be278dcd8fca81b235d7bb56fc
Florian Jasche <florian.jasche@gmail.com>
2021-07-21 15:06:09 +0200

    Updated appsettings json
    added bottoken and revoked bottoken

--- PrintAssistConsole/Config/TelegramBotConfiguration.cs
+++ PrintAssistConsole/Config/TelegramBotConfiguration.cs
@@ -2,6 +2,6 @@ namespace PrintAssistConsole
 {
     public static class TelegramBotConfiguration
     {
-        public readonly static string BotToken = "406651572:AAEuka4vG9HmnBLMUdYWrDr0j3aeMgeJoTM";
+        public readonly static string BotToken = "BotToken";
     }
 }
